3fb5774d57623c6c4ca7ac2bfefb8d357bfd4fb3
MagicAce1988 <52055351+MagicAce1988@users.noreply.github.com>
2020-01-12 19:23:02 +0000

    Removed commented code from app.js

--- test/src/App.js
+++ test/src/App.js
@@ -3,31 +3,7 @@ import LeftSide from "./components/leftside/LeftSide";
 import RightSide from "./components/rightside/RightSide";
 import "./App.css";
 
-export const appContext = React.createContext();
-
 function App() {
-
-  // const moveUser = id => {
-  //   let { leftUsers, rightUsers } = currentState;
-  //   rightUsers.push(leftUsers.find(user => user.influencer_id === id));
-  //   dispatch({
-  //     type: "CHANGE_RIGHT_USERS",
-  //     payload: rightUsers
-  //   });
-  // };
-
-  // const removeUser = id => {
-  //   let { rightUsers } = currentState;
-  //   rightUsers = rightUsers.filter(user => user.influencer_id !== id);
-  //   dispatch({
-  //     type: "CHANGE_RIGHT_USERS",
-  //     payload: rightUsers
-  //   });
-  // };
-
-  // let filteredUsers = currentState.leftUsers.filter(
-  //   user => !currentState.rightUsers.includes(user)
-  // );
 
   return (
       <div className="MyApp">
